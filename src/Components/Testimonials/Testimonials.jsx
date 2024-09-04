35ac0f72d2f167fd4e60c89509a221829892d38f
Gokul <89049577+AM-GokuL@users.noreply.github.com>
2024-09-05 02:29:59 +0530

    Add files via upload

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,96 @@
+import React, { useRef } from 'react'
+import './Testimonials.css'
+import next_icon from "../../Assets/next-icon.png"
+import back_icon from "../../Assets/back-icon.png";
+import user_1 from "../../Assets/user-1.png";
+import user_2 from "../../Assets/user-2.png";
+import user_3 from "../../Assets/user-3.png";
+import user_4 from "../../Assets/user-4.png";
+
+const Testimonials = () => {
+  const slider =useRef();
+  let tx=0;
+  const slideForward=()=>{
+     if(tx > -50)tx -= 25; 
+     slider.current.style.transform =`translateX(${tx}%)`;
+  }
+  const slideBackward=()=>{
+    if(tx < 0)tx += 25; 
+    slider.current.style.transform =`translateX(${tx}%)`;
+
+  }  
+
+
+  return (
+    <div className='testimonials'>
+       <img src={next_icon} alt="" className='next-btn' onClick={slideForward}/>
+       <img src={back_icon} alt="" className='back-btn' onClick={slideBackward}/>
+       <div className="slider">
+          <ul ref={slider}>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_1} alt="" />
+                        <div>
+                            <h3>Gokul</h3>
+                            <span>Madurai,India</span>
+                        </div>
+                    </div>
+                    <p>My university has provided a rich academic environment where 
+                                I've thrived intellectually and personally. The faculty's dedication 
+                                and the diverse student body have made my journey truly fulfilling and inspiring.
+                            </p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_2} alt="" />
+                        <div>
+                            <h3>Raghavv</h3>
+                            <span>Madurai,India</span>                           
+                        </div>
+                    </div>
+                    <p>My university has provided a rich academic environment where 
+                                I've thrived intellectually and personally. The faculty's dedication 
+                                and the diverse student body have made my journey truly fulfilling and inspiring.
+                            </p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_3} alt="" />
+                        <div>
+                            <h3>Kumar</h3>
+                            <span>Madurai,India</span>
+                        </div>
+                    </div>
+                    <p>My university has provided a rich academic environment where 
+                                I've thrived intellectually and personally. The faculty's dedication 
+                                and the diverse student body have made my journey truly fulfilling and inspiring.
+                            </p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_4} alt="" />
+                        <div>
+                            <h3>Meenatchi</h3>
+                            <span>Madurai,India</span>
+                        </div>
+                    </div>
+                    <p>My university has provided a rich academic environment where 
+                        I've thrived intellectually and personally. The faculty's dedication 
+                        and the diverse student body have made my journey truly fulfilling and inspiring.
+                    </p>
+                </div>
+            </li>
+          </ul>
+       </div> 
+    </div>
+  )
+}
+
+export default Testimonials
